902e07dbbf21cd006271e926537c5a998097c3fe
Thiago Gabriel <thiago.gabriel051@gmail.com>
2023-06-28 18:10:23 -0300

    Validações de login

--- backend/middlewares/userValidations.js
+++ backend/middlewares/userValidations.js
@@ -20,8 +20,8 @@ const userCereateValidation = () => {
         body('confirmpassword')
             .isString()
             .withMessage('A confirmação de senha é obrigatória.')
-            .custom((value, {req}) => {
-                if(value !== req.body.password) {
+            .custom((value, { req }) => {
+                if (value !== req.body.password) {
                     throw new Error('As Senhas não são iguais.')
                 }
                 return true
@@ -29,6 +29,20 @@ const userCereateValidation = () => {
     ]
 }
 
+const loginValidation = () => {
+    return [
+        body('email')
+            .isString()
+            .withMessage('O e-mail é obrigatório.')
+            .isEmail()
+            .withMessage('Insira um e-mail válido.'),
+        body('password')
+            .isString()
+            .withMessage('A senha é obrigatória.')
+    ]
+}
+
 module.exports = {
     userCereateValidation,
+    loginValidation,
 }
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -2,13 +2,17 @@ const express = require('express')
 const router = express.Router()
 
 // Controller
-const { register } = require('../controller/UserController')
+const { register, login } = require('../controller/UserController')
 
 // Middlewares
 const validate = require('../middlewares/handleValidation')
-const { userCereateValidation } = require('../middlewares/userValidations')
+const {
+    userCereateValidation,
+    loginValidation
+} = require('../middlewares/userValidations')
 
 // Routes
 router.post('/register', userCereateValidation(), validate, register)
+router.post('/login', loginValidation(), validate, login)
 
 module.exports = router
