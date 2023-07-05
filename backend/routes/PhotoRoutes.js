fdf7b0e7fdc0bc22db209a91c1767d33afa6b818
Thiago Gabriel <thiago.gabriel051@gmail.com>
2023-07-05 11:55:53 -0300

    Resgatando todas as fotos

--- backend/routes/PhotoRoutes.js
+++ backend/routes/PhotoRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 // Controller
-const { insertPhoto, deletePhoto } = require('../controller/PhotoControler')
+const { insertPhoto, deletePhoto, getAllPhotos } = require('../controller/PhotoControler')
 
 // Middlewares
 const { photoInsertValidation } = require('../middlewares/photoValidation')
@@ -19,10 +19,7 @@ router.post('/',
     validate,
     insertPhoto
 )
-
-router.delete('/:id',
-    authGuard,
-    deletePhoto
-)
+router.delete('/:id', authGuard, deletePhoto)
+router.get('/', authGuard, getAllPhotos)
 
 module.exports = router
