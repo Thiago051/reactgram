0b0e54b8f29687324ee24e6c24428423ca8881aa
Thiago Gabriel <thiago.gabriel051@gmail.com>
2023-07-10 11:58:10 -0300

    Configurando React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,29 @@
 import './App.css'
 
+// Router
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom'
+
+// Pages
+import Home from './pages/Home/Home'
+import Login from './pages/Auth/Login'
+import Register from './pages/Auth/Register'
+
 function App() {
 
   return (
-    <div>
-      <h1>ReactGram</h1>
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>ReactGram</h1>
+        </div>
+    )
+}
+
+export default Home
