00e8bcefe3adf9296253ac09bf07edeaffdeb443
Thiago Gabriel <thiago.gabriel051@gmail.com>
2023-07-10 12:11:42 -0300

    Criando component de NavBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,16 +13,22 @@ import Home from './pages/Home/Home'
 import Login from './pages/Auth/Login'
 import Register from './pages/Auth/Register'
 
+// Components
+import NavBar from './components/NavBar/NavBar'
+import Footer from './components/Footer/Footer'
+
 function App() {
 
   return (
     <div className='App'>
       <BrowserRouter>
+        <NavBar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   )
