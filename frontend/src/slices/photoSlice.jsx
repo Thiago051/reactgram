c0e6eb8a7cbb8c2e6e6d93932098c61779472368
Thiago Gabriel <thiago.gabriel051@gmail.com>
2023-08-14 18:43:15 -0300

    Slice de exclusão de foto

--- frontend/src/slices/photoSlice.jsx
+++ frontend/src/slices/photoSlice.jsx
@@ -32,11 +32,23 @@ export const getUserPhotos = createAsyncThunk(
     'photo/userphotos',
     async (id, thunkAPI) => {
         const token = thunkAPI.getState().auth.user.token
-
         const data = await photoService.getUserPhotos(id, token)
 
-        console.log(data)
-        console.log(data.errors)
+        return data
+    }
+)
+
+// delete a photo
+export const deletePhoto = createAsyncThunk(
+    'photo/delete',
+    async (id, thunkAPI) => {
+        const token = thunkAPI.getState().auth.user.token
+        const data = await photoService.deletePhoto(id, token)
+
+        // check for errors
+        if (data.errors) {
+            return thunkAPI.rejectWithValue(data.errors[0])
+        }
 
         return data
     }
@@ -83,6 +95,27 @@ export const photoSlice = createSlice({
                     state.success = true;
                     state.error = null;
                     state.photos = action.payload;
+                })
+            .addCase(deletePhoto.pending,
+                (state) => {
+                    state.loading = true
+                    state.error = false
+                })
+            .addCase(deletePhoto.fulfilled,
+                (state, action) => {
+                    state.loading = false
+                    state.sucsses = true
+                    state.error = null
+                    state.photos = state.photos.filter((photo) => {
+                        return photo._id !== action.payload.id
+                    })
+                    state.message = action.payload.message
+                })
+            .addCase(deletePhoto.rejected,
+                (state, action) => {
+                    state.loading = false
+                    state.error = action.payload
+                    state.photo = {}
                 })
     }
 }
