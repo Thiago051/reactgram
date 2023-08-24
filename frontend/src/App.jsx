645435e3fb2df5bd7261c12786f47660b1ffd424
Thiago Gabriel <thiago.gabriel051@gmail.com>
2023-08-24 17:49:16 -0300

    Criando página de busca

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,6 +18,7 @@ import Register from './pages/Auth/Register'
 import EditProfile from './pages/EditProfile/EditProfile'
 import Profile from './pages/Profile/Profile'
 import Photo from './pages/Photo/Photo'
+import Search from './pages/Search/Search'
 
 // Components
 import NavBar from './components/NavBar/NavBar'
@@ -53,6 +54,9 @@ function App() {
             />
             <Route path='/register'
               element={!auth ? <Register /> : <Navigate to={'/'} />}
+            />
+            <Route path='/search'
+              element={auth ? <Search /> : <Navigate to={'/login'} />}
             />
              <Route path='/photos/:id'
               element={auth ? <Photo /> : <Navigate to={'/login'} />}
---
+++ frontend/src/pages/Search/Search.jsx
@@ -0,0 +1,21 @@
+import './Serach.css'
+
+// Hooks
+import { useEffect } from 'react'
+import { UseSelector, useDispatch } from 'react-redux'
+import { useResetComponentMessage } from '../../hooks/useResetComponentMessage'
+
+// Components
+import LikeContainer from '../../components/LikeContainer/LikeContainer'
+import PhotoItem from '../../components/PhotoItem/PhotoItem'
+import { Link } from 'react-router-dom'
+
+// Redux
+
+const Search = () => {
+    return (
+        <div>Search</div>
+    )
+}
+
+export default Search
