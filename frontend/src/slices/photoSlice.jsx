3b3f3568547ff058b59c87e93a1a7f34934c4c6a
Thiago Gabriel <thiago.gabriel051@gmail.com>
2023-08-24 18:07:08 -0300

    Slice de busca

--- frontend/src/slices/photoSlice.jsx
+++ frontend/src/slices/photoSlice.jsx
@@ -130,6 +130,16 @@ export const getPhotos = createAsyncThunk(
     }
 )
 
+// search photos by title
+export const searchPhotos = createAsyncThunk(
+    'photo/search',
+    async(query, thunkAPI) => {
+        const token = thunkAPI.getState().auth.user.token
+        const data = await photoService.searchPhotos(query, token)
+        return data
+    }
+)
+
 export const photoSlice = createSlice({
     name: 'photo',
     initialState,
@@ -274,6 +284,18 @@ export const photoSlice = createSlice({
                     state.error = null;
                     state.photos = action.payload;
                 })
+                .addCase(searchPhotos.pending,
+                    (state) => {
+                        state.loading = true;
+                        state.error = null;
+                    })
+                .addCase(searchPhotos.fulfilled,
+                    (state, action) => {
+                        state.loading = false;
+                        state.success = true;
+                        state.error = null;
+                        state.photos = action.payload;
+                    })
     }
 }
 )
